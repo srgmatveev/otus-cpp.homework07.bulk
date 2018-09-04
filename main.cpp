71dc7fe4b0bee3ac582d0b48472d14558ef0d939
Sergio <srgmatveev@gmail.com>
2018-09-04 15:16:39 +0300

    bulk main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <cstddef>
+#include <memory>
+#include <exception>
 #include "bulk.h"
 #include "utils.h"
-#include <memory>
 
 int main(int argc, char const *argv[])
 {
     std::size_t chunk_size{0};
-    if (argc != 2)
+    try
     {
-        std::cerr << "Параметр N передается как единственный "
-                  << "параметр командной строки в виде целого числа" << std::endl;
-        std::cerr << "Пример: bulk 15" << std::endl;
-    }
+        if (argc != 2)
+        {
+            std::cout << "Параметр N передается как единственный "
+                      << "параметр командной строки в виде целого числа" << std::endl;
+            std::cout << "Пример: bulk 15" << std::endl;
+            return 0;
+        }
 
-    if (!is_numeric<char const *, elem_traits<std::size_t>::value_type>(argv[1]))
+        if (!is_numeric<char const *, elem_traits<std::size_t>::value_type>(argv[1]))
+        {
+            return 0;
+        }
+
+        chunk_size = std::atoi(argv[1]);
+
+        auto ptrBulkRead = BulkReadCmd::create(chunk_size);
+        auto ptrToConsolePrint = ToConsolePrint::create(std::cout);
+        auto ptrToFilePrint = ToFilePrint::create();
+
+        ptrBulkRead->subscribe(ptrToConsolePrint.get());
+        ptrBulkRead->subscribe(ptrToFilePrint.get());
+        ptrBulkRead->process(std::cin);
+        ptrBulkRead->unsubscribe(ptrToFilePrint.get());
+        ptrBulkRead->unsubscribe(ptrToConsolePrint.get());
+    }
+    catch (std::exception &e)
     {
-        return 0;
+        std::cerr << e.what() << std::endl;
     }
-
-    chunk_size = std::atoi(argv[1]);
-
-    auto ptrBulkRead = BulkReadCmd::create(chunk_size);
-    auto ptrToConsolePrint = ToConsolePrint::create(std::cout);
-    auto ptrToFilePrint = ToFilePrint::create();
-
-    ptrBulkRead->subscribe(ptrToConsolePrint.get());
-    ptrBulkRead->subscribe(ptrToFilePrint.get());
-    ptrBulkRead->process(std::cin);
-    ptrBulkRead->unsubscribe(ptrToFilePrint.get());
-    ptrBulkRead->unsubscribe(ptrToConsolePrint.get());
-
     return 0;
 }
